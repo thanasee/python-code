06e2e85e39330ffa18157c71d52c90fc0d769525
Thanasee Thanasarnsurapong <168052927+thanasee@users.noreply.github.com>
2026-05-13 02:39:30 +0100

    Enhance documentation and adjust cutoff margin
    Added detailed docstrings for functions and updated cutoff margin.

--- enforceIFC.py
+++ enforceIFC.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 from sys import argv, exit
 from glob import glob
+import h5py
 from ase.io import read
 from hiphive import ClusterSpace, ForceConstantPotential, enforce_rotational_sum_rules
 from hiphive.force_constants import ForceConstants
@@ -25,53 +26,146 @@ This script was developed by Thanasee Thanasarnsurapong.
 
 
 def read_structure(filename):
-    """Read the structure from a file."""
+    """
+    Read an atomic structure from a file.
+
+    Parameters
+    ----------
+    filename : str
+        Path to the structure file (e.g., POSCAR, SPOSCAR).
+
+    Returns
+    -------
+    ase.Atoms
+        Atomic structure read from the file.
+    """
     return read(filename)
 
 
 def build_cluster_space(primitive, supercell, cutoff_margin=0.01):
+    """
+    Build a cluster space for the given primitive and supercell structures.
+
+    Parameters
+    ----------
+    primitive : ase.Atoms
+        Primitive unit cell structure.
+    supercell : ase.Atoms
+        Supercell structure used to estimate the maximum cutoff radius.
+    cutoff_margin : float, optional
+        Margin subtracted from the maximum cutoff to avoid incomplete
+        neighbor shells at the boundary, in Angstrom. Default is 0.01.
+
+    Returns
+    -------
+    hiphive.ClusterSpace
+        Cluster space constructed with a single 2-body cutoff.
+    """
     max_cutoff = estimate_maximum_cutoff(supercell) - cutoff_margin
     cutoffs = [max_cutoff]
     cluster_space = ClusterSpace(primitive, cutoffs)
     return cluster_space
 
 def read_phonopy_fc2(supercell, fc_file="FORCE_CONSTANTS", fc_format="text"):
-    return ForceConstants.read_phonopy(
-        supercell,
-        fc_file,
-        format=fc_format,
-    )
+    """
+    Read phonopy FC2 force constants from a file.
+
+    Parameters
+    ----------
+    supercell : ase.Atoms
+        Supercell structure corresponding to the force constants.
+    fc_file : str, optional
+        Path to the force constants file. Default is 'FORCE_CONSTANTS'.
+    fc_format : str, optional
+        File format, either 'text' or 'hdf5'. Default is 'text'.
+
+    Returns
+    -------
+    hiphive.force_constants.ForceConstants
+        Force constants object read from the file.
+    """
+    return ForceConstants.read_phonopy(supercell, fc_file, format=fc_format)
 
 def project_fc2_to_parameters(force_constants, cluster_space):
+    """
+    Project FC2 force constants onto the cluster space parameters.
+
+    Parameters
+    ----------
+    force_constants : hiphive.force_constants.ForceConstants
+        Force constants to project.
+    cluster_space : hiphive.ClusterSpace
+        Cluster space defining the basis for projection.
+
+    Returns
+    -------
+    numpy.ndarray
+        Parameter vector representing the force constants in the cluster space.
+    """
     return extract_parameters(force_constants, cluster_space)
 
 
-def apply_rotational_sum_rules(
-    cluster_space,
-    parameters,
-    sum_rules=("Huang", "Born-Huang"),
-):
-    return enforce_rotational_sum_rules(
-        cluster_space,
-        parameters,
-        list(sum_rules),
-    )
+def apply_rotational_sum_rules(cluster_space, parameters, sum_rules=("Huang", "Born-Huang")):
+    """
+    Enforce rotational sum rules on the force constant parameters.
+
+    Parameters
+    ----------
+    cluster_space : hiphive.ClusterSpace
+        Cluster space corresponding to the parameters.
+    parameters : numpy.ndarray
+        Parameter vector to enforce sum rules on.
+    sum_rules : tuple of str, optional
+        Sum rules to enforce. Default is ('Huang', 'Born-Huang').
+
+    Returns
+    -------
+    numpy.ndarray
+        Parameter vector with rotational sum rules enforced.
+    """
+    return enforce_rotational_sum_rules(cluster_space, parameters, list(sum_rules))
 
 
 def parameters_to_force_constants(cluster_space, parameters, supercell):
+    """
+    Reconstruct force constants from cluster space parameters.
+
+    Parameters
+    ----------
+    cluster_space : hiphive.ClusterSpace
+        Cluster space defining the force constant basis.
+    parameters : numpy.ndarray
+        Parameter vector to reconstruct force constants from.
+    supercell : ase.Atoms
+        Supercell structure to evaluate the force constants on.
+
+    Returns
+    -------
+    hiphive.force_constants.ForceConstants
+        Reconstructed force constants object.
+    """
     fcp = ForceConstantPotential(cluster_space, parameters)
     return fcp.get_force_constants(supercell)
 
 
 def write_phonopy_fc2(force_constants, fc_file, fc_format="text"):
-    force_constants.write_to_phonopy(
-        fc_file,
-        format=fc_format,
-    )
+    """
+    Write FC2 force constants to a phonopy-compatible file.
+
+    Parameters
+    ----------
+    force_constants : hiphive.force_constants.ForceConstants
+        Force constants object to write.
+    fc_file : str
+        Path to the output file.
+    fc_format : str, optional
+        File format, either 'text' or 'hdf5'. Default is 'text'.
+    """
+    force_constants.write_to_phonopy(fc_file, format=fc_format)
 
 
 def main():
-    
+    """Parse arguments, read structures and force constants, apply sum rules, and write modified force constants."""
     if '-h' in argv or '--help' in argv or len(argv) > 3:
         usage()
     
@@ -103,47 +197,19 @@ def main():
     )
     output_format = "hdf5" if output_fc_file.endswith(".hdf5") else "text"
 
-    cutoff_margin = 0.001
+    cutoff_margin = 1e-5
     sum_rules = ("Huang", "Born-Huang")
     # ---------------------
 
     primitive = read_structure(primitive_file)
     supercell = read_structure(supercell_file)
 
-    cluster_space = build_cluster_space(
-        primitive,
-        supercell,
-        cutoff_margin,
-    )
-
-    force_constants = read_phonopy_fc2(
-        supercell,
-        input_fc_file,
-        input_format,
-    )
-
-    parameters = project_fc2_to_parameters(
-        force_constants,
-        cluster_space,
-    )
-
-    parameters_rot = apply_rotational_sum_rules(
-        cluster_space,
-        parameters,
-        sum_rules,
-    )
-
-    force_constants_rot = parameters_to_force_constants(
-        cluster_space,
-        parameters_rot,
-        supercell,
-    )
-
-    write_phonopy_fc2(
-        force_constants_rot,
-        output_fc_file,
-        output_format,
-    )
+    cluster_space = build_cluster_space(primitive, supercell, cutoff_margin)
+    force_constants = read_phonopy_fc2(supercell, input_fc_file, input_format)
+    parameters = project_fc2_to_parameters(force_constants, cluster_space)
+    parameters_rot = apply_rotational_sum_rules(cluster_space, parameters, sum_rules)
+    force_constants_rot = parameters_to_force_constants(cluster_space, parameters_rot, supercell)
+    write_phonopy_fc2(force_constants_rot, output_fc_file, output_format)
 
 
 if __name__ == "__main__":
